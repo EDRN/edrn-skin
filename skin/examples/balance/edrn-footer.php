762001eb32c2e168a03ad7de5858d39d7360ff8c
Andrew Hart <afh@andrewfhart.com>
2011-11-02 20:59:21 +0000

    add example header and footer files suitable for drop-in use with OODT Balance applications

---
+++ skin/examples/balance/edrn-footer.php
@@ -0,0 +1,19 @@
+<?php
+?>
+</div>
+<div id="footer">
+	A Service of the National Cancer Institute<br/><br/>
+	<a href="http://hhs.gov" >
+		<img src="<?php echo SITE_ROOT?>/static/edrn-skin/img/footer_hhs.gif" alt="Department of Health and Human Services"/>
+	</a>
+	<a href="http://nih.gov" >
+
+		<img src="<?php echo SITE_ROOT?>/static/edrn-skin/img/footer_nih.gif" style="margin-left:12px;" alt="National Institutes of Health"/>
+	</a>
+	<a href="http://usa.gov">
+		<img src="<?php echo SITE_ROOT?>/static/edrn-skin/img/footer_usagov.gif"/>
+	</a>
+</div>
+
+</body>
+</html>
---
+++ skin/examples/balance/edrn-header.php
@@ -0,0 +1,42 @@
+<?php
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<link rel="stylesheet" type="text/css" href="<?php echo SITE_ROOT?>/static/edrn-skin/css/edrn-informatics.css"/>
+	<link rel="stylesheet" type="text/css" href="<?php echo SITE_ROOT?>/static/css/site.css"/>
+	<script type="text/javascript" src="https://www.google.com/jsapi"></script>
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
+	
+</head>
+<body>
+<div id="page">
+	<div id="ncibanner">
+		<div id="ncibanner-inner">
+		  <a href="http://www.cancer.gov/"><h2 class="ncilogo">National Cancer Institute</h2></a>
+		  <a href="http://www.cancer.gov/"><h2 class="cdglogo">www.cancer.gov</h2></a>
+		  <a href="http://www.nih.gov/"><h2 class="nihlogo">National Institutes of Health</h2></a>
+		</div>
+	</div>
+	<br class="clr"/>
+
+	<div id="edrnlogo">
+		<h1  class="header-title"><img id="edrnlogo-logo" src="<?php echo SITE_ROOT?>/static/edrn-skin/img/edrn-logo.png"/>Early Detection Research Network</h1>
+		<h2  class="header-title">Biomarkers: The Key to Early Detection</h2>
+	</div>
+	<div id="dcplogo">
+		<h2 class="dcplogo"><a href="http://prevention.cancer.gov">Division of Cancer Prevention</a></h2>
+	</div>
+
+	<div class="userdetails">
+		Not Logged In. &nbsp; <a href="<?php echo SITE_ROOT?>/login">Log In</a>	</div>
+	<h2 class="app-title" style="margin-top:146px;">[APPLICATION NAME HERE]</h2>
+	<div class="menu">
+		<!-- Breadcrumbs Area -->
+		<div id="breadcrumbs"/>
+			<ul><li><a href="<?php echo SITE_ROOT?>/">Home</a></li></ul>
+		</div><!-- End Breadcrumbs -->
+	</div>
+	
+	
